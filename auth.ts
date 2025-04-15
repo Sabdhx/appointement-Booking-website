feda41f08368fa395967d1c44a85ad56d84777af
Sabdhx <syedaliabdullahshah5@gmail.com>
2025-04-15 09:57:54 +0500

    changes

--- auth.ts
+++ auth.ts
@@ -1,14 +1,47 @@
-import NextAuth from "next-auth"
-import Credentials from "next-auth/providers/credentials"
- 
+import NextAuth, { CredentialsSignin } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import User from "./models/user";
+import { DBConnect } from "./app/lib/DB";
+import { compare } from "bcryptjs";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Credentials({
       name: "Credentials",
       credentials: {
-        name: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" }
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
       },
+       authorize:async(credentials:any)=>{
+        const email = credentials.email as string;
+        const password = credentials.password 
+
+        if(!email || !password){
+          throw new CredentialsSignin("please provide both email and password")
+        }
+        await DBConnect()
+          const findingUser = await User.find({email}).select("+password +role")
+
+          if(!findingUser){
+            throw new Error("invalid email or password")
+          }
+          if(!password){
+            throw new Error("invalid password")
+          }
+
+          const comparingPassword = await compare(password, findingUser.password);
+
+          if(!comparingPassword){
+            throw new Error("password did not matched")
+          }
+
+          const data={
+            username: findingUser.username,
+            role:findingUser.role,
+            email:findingUser.email,
+          }
+      },
+      
     }),
-  ],
-})
+  ]
+});
