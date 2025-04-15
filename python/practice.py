b92cf97250ce0bdf8ab8d587825c9e531a8d36aa
Sabdhx <syedaliabdullahshah5@gmail.com>
2025-04-16 01:50:01 +0500

    just for practice added a python file

---
+++ python/practice.py
@@ -0,0 +1,40 @@
+# firstValue = int(input("what is your age "))
+# secondValue = int(input("what is your age "))
+# method = input("what kind of method do you want ")
+# result = ""
+# if method == "multiplication": result = firstValue * secondValue
+# elif method == "subtration": result = secondValue - firstValue
+# elif method == "division": result = firstValue / secondValue
+# elif method == "addition": result = firstValue + secondValue
+
+# newvalue = int(input("more value to divide or multiply or any operation: "))
+# newOperation = input("new Operation to apply")
+
+# if newOperation == "multiplication": result = newvalue * result
+# elif newOperation == "subtration": result = newvalue - result
+# elif newOperation == "division": result = newvalue / result
+# elif newOperation == "addition": result = newvalue + result
+
+
+# print(round(result))
+
+
+# Array = ["orange", "banana","apple","strawberry"]
+# print(len(Array)) 
+
+
+# variable = bool("hey nate hows life")
+# print(variable)  # Output: True
+
+
+# my_tuple = ("hello",1,2,3,[3,5,"henna"], {"hey":"bro wsss up"})
+# print(my_tuple[5]["hey"])
+
+
+# my_tuple = (1,2,3,4,5)
+# filteredTuple = filter(lambda  x:x % 2 == 0, my_tuple)
+# print(list(filteredTuple))
+
+# my_tuple = [1,2,3,4,5,6]
+# new_tuple = map(lambda x:x*2 , my_tuple)
+# print(list(new_tuple))
